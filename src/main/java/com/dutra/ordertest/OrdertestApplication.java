3533acc8a0de498f215e5a6f982c27719ff5cf41
luisdutra <luis.dutra@softfocus.com.br>
2025-04-28 21:40:24 -0300

    add swagger

--- src/main/java/com/dutra/ordertest/OrdertestApplication.java
+++ src/main/java/com/dutra/ordertest/OrdertestApplication.java
@@ -2,8 +2,10 @@ package com.dutra.ordertest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 //@EnableScheduling
 public class OrdertestApplication {
 
--- src/main/java/com/dutra/ordertest/controller/OrderController.java
+++ src/main/java/com/dutra/ordertest/controller/OrderController.java
@@ -3,16 +3,18 @@ package com.dutra.ordertest.controller;
 import com.dutra.ordertest.dto.OrderDTO;
 import com.dutra.ordertest.model.Order;
 import com.dutra.ordertest.service.OrderService;
-import java.util.List;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
+@Api(tags = "Order Controller", description = "endpoints for orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -22,6 +24,11 @@ public class OrderController {
     }
 
     @PostMapping
+    @ApiOperation(value = "create new order")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "create order with success"),
+            @ApiResponse(code = 400, message = "invalid data")
+    })
     public ResponseEntity<Order> createOrder(@RequestBody OrderDTO orderDTO) {
         Order order = orderService.createOrder(orderDTO);
         return ResponseEntity.ok(order);
